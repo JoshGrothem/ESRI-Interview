3f94e369c91b3c329c46be2d591cd49d281fb60f
Joshua Grothem <joshua.grothem@gmail.com>
2023-03-23 20:27:32 -0700

    Update anagram.js
    Implemented bubble sort

--- anagram.js
+++ anagram.js
@@ -3,14 +3,26 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
+var isAnagram = function (s, t) {
     let charS = Array.from(s);
     let charT = Array.from(t);
 
-    charS.sort();
-    charT.sort();
+    if (charS.length != charT.length) {
+        return false;
+    }
 
-    return charS.toString()===charT.toString();
+    for (let i = 0; i < charS.length; i++) {
+        for (let j = 0; j < charS.length - i - 1; j++) {
+            if (charS[j + 1] < charS[j]) {
+                [charS[j + 1], charS[j]] = [charS[j], charS[j + 1]];
+            }
+            if (charT[j + 1] < charT[j]) {
+                [charT[j + 1], charT[j]] = [charT[j], charT[j + 1]];
+            }
+        }
+    }
+
+    return charS.toString() === charT.toString();
 };
 
 console.log(isAnagram("rat", "tar"));

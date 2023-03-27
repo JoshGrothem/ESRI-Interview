b25ecf11ebedf80bdb0fdbe209f52766bee92c68
Joshua Grothem <joshua.grothem@gmail.com>
2023-03-27 10:28:23 -0700

    Update sodoku.js
    Implemented new solution for checking validity of box (3x3 grid)

--- sodoku.js
+++ sodoku.js
@@ -3,44 +3,35 @@
  * @return {boolean}
  */
 var isValidSudoku = function (board) {
-    //checking row and col for validity
+
     for (let i = 0; i < board.length; i++) {
         let row = new Set();
         let col = new Set();
+        let box = new Set();
         for (let j = 0; j < board.length; j++) {
             let r = board[i][j];
             let c = board[j][i];
+            let b = board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)]
 
             if (r != ".") {
                 if (row.has(r)) {
                     return false;
                 }
                 row.add(r);
-                if (c != ".") {
-                    if (col.has(c)) {
-                        return false;
-                    }
-                    col.add(c);
-                }
             }
-        }
-    }
+            if (c != ".") {
+                if (col.has(c)) {
+                    return false;
+                }
+                col.add(c);
+            }
+            if (b != ".") {
+                if (box.has(b)) {
+                    return false;
+                }
+                box.add(b);
+            }
 
-    //checking 3x3 boxes
-    for (let i = 0; i < 9 - 2; i += 3) {
-        let box = new Set();
-        for (let j = 0; j < 9 - 7; j += 3) {
-            for (let k = 0; k < 3; k++) {
-                for (let l = 0; l < 3; l++) {
-                    let b = board[i + k][j + l];
-                    if (b != ".") {
-                        if (box.has(b)) {
-                            return false;
-                        }
-                        box.add(b);
-                    }
-                }
-            }
         }
     }
 
